d636db5e4d176e23d78245cebb9d1ef4958a7a51
ciphercleo <cleodelfabbro@gmail.com>
2026-07-23 19:26:25 -0300

    Automação com Page Objects

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,11 +1,13 @@
 /// <reference types="cypress"/>
 import { faker } from '@faker-js/faker';
+import cadastroPage from '../support/pages/cadastro-page'
 
 describe('Funcionalidade: Cadastro no hub de leitura', () => {
 
     beforeEach(() => {
-        cy.visit("register.html")
+        cadastroPage.visitarPaginaCadastro()
     });
+
     it('Deve fazer cadastro com sucesso, usando função JS', () => {
         let email = `teste${Date.now()}@teste.com`
         cy.get('#name').type('Nome teste')
@@ -46,4 +48,20 @@ describe('Funcionalidade: Cadastro no hub de leitura', () => {
         )
         cy.url().should('include', 'dashboard')
     });
+
+    it('Deve fazer cadastro com sucesso usando Page Objects', () => {
+        let email = `teste${Date.now()}@teste.com`
+        cadastroPage.preencherCadastro('Cleo', email, '11889234923', 'senha123', 'senha123')
+        cy.url().should('include', 'dashboard')
+    });
+
+    it('Deve validar mensagem ao tentar cadastrar sem preencher nome', () => {
+        cadastroPage.preencherCadastro('', 'cleo@teste.com', '1183983289943', 'senha123', 'senha123')
+        cy.get(':nth-child(1) > .invalid-feedback').should('contain', 'Nome deve ter pelo menos 2 caracteres')
+    });
+    it.only('Deve validar mensagem ao tentar cadastrar sem preencher nome', () => {
+        cadastroPage.preencherCadastro('Cleo', '', '1183983289943', 'senha123', 'senha123')
+        cy.get('#register-form > :nth-child(2) > .invalid-feedback').should('contain', 'Email válido é obrigatório')
+    });
 });
+
--- cypress/e2e/catalogo-busca.cy.js
+++ cypress/e2e/catalogo-busca.cy.js
@@ -14,6 +14,22 @@ describe('Funcionalidade: Busca no catálogo', () => {
 
     it('Deve fazer a busca de um livro do arquivo de massa de dados ', () => {
         cy.get('#search-input').type(catalogo[2].livro)
-        cy.get('.card-title').should('contain', '1984')
+        cy.get('.card-title').should('contain', catalogo[2].livro)
+    });
+
+    it('Deve fazer a busca de um livro usando fixture', () => {
+        cy.fixture('livros').then((cat) => {
+            cy.get('#search-input').type(cat[2].livro)
+            cy.get('.card-title').should('contain', cat[2].livro)
+        })
+    });
+
+    it.only('Deve validar todos os livros da lista', () => {
+        cy.fixture('livros').then((cat) => {
+            cat.forEach(item => {
+                 cy.get('#search-input').clear().type(item.livro)
+                 cy.get('.card-title').should('contain', item.livro)
+            })
+        })
     });
 });
